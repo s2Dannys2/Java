ee65b19eb4dde06299d1477441771201498c455c
danie <adreandanielly@gmail.com>
2025-07-10 08:36:52 -0300

    primeiro commit

---
+++ EstruturaSequecial/src/Teoria/Variaveis.java
@@ -0,0 +1,22 @@
+package Teoria;
+public class Variaveis {
+    public static void main(String[] args) throws Exception {
+        /*
+         Sintaxe para declaração de uma variável:
+         <tipo> <nome> = <valor inicial>;
+         
+         Exemplos abaixo:
+         */
+        int idade = 25;
+        double altura = 1.68;
+        char sexo = 'F';
+        /*
+         Uma variável possui:
+            *Nome
+            *Tipo
+            *Valor
+            *Endereço
+         */
+        
+    }
+}
